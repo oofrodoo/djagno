d9a190e67f8e32347a13785ef96aa66c9f60a8cb
oofrodoo <oofrodoo12@gmail.com>
2025-07-02 12:22:49 +0545

    new

--- firstproject/post/views.py
+++ firstproject/post/views.py
@@ -1,10 +1,15 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-# from post.models import post
+from post.models import Post
 # Create your views here.
 
 def post(request):
     return HttpResponse('Hello World')
 
 def list_post (request):
+    posts = Post.objects.all()
     return render(request, "post/list.html")
+    context = {
+        "posts":posts
+    }
+
